728814cce847f57b16c300c2e1a8dadf2cb762ea
mazharmnzoor4227-beep <mazharmnzoor4227@gmail.com>
2026-09-25 22:36:24 +0500

    Add Sonify track model

---
+++ app/src/main/java/com/sonify/music/model/Track.kt
@@ -0,0 +1,11 @@
+package com.sonify.music.model
+
+data class Track(
+    val id: String,
+    val title: String,
+    val artist: String,
+    val artworkUrl: String,
+    val streamUrl: String = "",
+    val source: String = "",
+    val durationMs: Long = 0L
+)
